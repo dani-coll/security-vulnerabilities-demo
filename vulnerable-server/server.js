36cacc2ca15d57c424e4f7cf77616704b2d9f6e0
Coll <daniel.coll@dynatrace.com>
2025-10-23 11:19:31 +0200

    Fix frontend

--- vulnerable-server/server.js
+++ vulnerable-server/server.js
@@ -20,7 +20,7 @@ app.options("*", (req, res) => {
 });
 
 // Serve static files from my-app/dist
-const frontendPath = path.join(__dirname, "../my-app/dist");
+const frontendPath = path.join(__dirname, "../vulnerable-frontend/dist");
 console.log("Frontend path:", frontendPath);
 console.log("Frontend path exists:", require('fs').existsSync(frontendPath));
 app.use(express.static(frontendPath));
